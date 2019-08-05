1f4fdb199c5c4e57f4c2b41a863ac0f7f4e09ef4
xsj <shijie.xu@ucarinc.com>
2019-08-05 11:59:46 +0800

    init

---
+++ zuul/src/main/java/com/lr/zuul/ZuulApplication.java
@@ -0,0 +1,15 @@
+package com.lr.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+}
