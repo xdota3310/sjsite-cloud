e0b3610a48328963d3b21f04c0e6247c30e70fdf
xsj <shijie.xu@ucarinc.com>
2019-08-06 18:37:44 +0800

    配置config

--- configcenter/src/main/java/com/lr/configcenter/ConfigcenterApplication.java
+++ configcenter/src/main/java/com/lr/configcenter/ConfigcenterApplication.java
@@ -2,9 +2,11 @@ package com.lr.configcenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigcenterApplication {
 
--- zuul/src/main/java/com/lr/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/lr/zuul/ZuulApplication.java
@@ -1,10 +1,13 @@
 package com.lr.zuul;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @EnableEurekaClient
+//@EnableConfig
 @SpringBootApplication
 public class ZuulApplication {
 
