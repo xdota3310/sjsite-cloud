10fb19c5991b32cf82d41582232f5f6f6d164a06
shijie.xu <xsj1012@foxmail.com>
2019-12-09 23:42:55 +0800

    blog

---
+++ blog/src/test/java/com/lr/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lr.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
